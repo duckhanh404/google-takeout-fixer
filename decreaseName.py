cdda0c9e6bc4f1f77356546d0bd9895db7c25b68
duckhanh404 <duckhanh.404@gmail.com>
2026-01-08 23:25:53 +0700

    Version 4.0

--- decreaseName.py
+++ decreaseName.py
@@ -2,47 +2,73 @@ from pathlib import Path
 import unicodedata
 from typing import List, Optional, Any
 from pathlib import Path
+import json
+from metadata import get_all_media, get_all_json
 
 
-from jsonWork import *
 
 
 
 
+# def find_matching_json(
+#     path: str|Path,
+#     media_filename: str,
+#     json_files: List[str]
+# ) -> Optional[str]:
+#     """
+#     Tìm file json có tên bắt đầu bằng media name (giảm dần từng ký tự).
+#     Trả về tên file json nếu tìm thấy, ngược lại trả None.
+#     """
+
+#     # Chuẩn hóa Unicode (rất quan trọng với tiếng Việt)
+#     media_filename = unicodedata.normalize("NFC", media_filename)
+#     json_files = [unicodedata.normalize("NFC", f) for f in json_files]
+
+#     # Bỏ extension media
+
+#     base_name = Path(media_filename).stem
+#     media_ext = Path(media_filename).suffix.lower()
+
+#     # Giảm dần từng ký tự trong media name để tìm
+#     for i in range(len(base_name), 0, -1):
+#         prefix = base_name[:i]
+#         for json_file in json_files:
+#             # Nằm ở đầu
+#             if json_file.startswith(prefix):
+#                 try:
+#                     json_path = Path(path, json_file)
+#                     with open(json_path, "r", encoding="utf-8") as f:
+#                         data = json.load(f)
+#                 except Exception:
+#                     continue
+#                 title = data.get("title")
+#                 if Path(title).suffix.lower() == media_ext:
+#                     return json_file
+
+#     return None
 
 def find_matching_json(
-    path: str|Path,
+    json_dict : dict[str, Any], # json:title
     media_filename: str,
-    json_files: List[str]
 ) -> Optional[str]:
     """
     Tìm file json có tên bắt đầu bằng media name (giảm dần từng ký tự).
     Trả về tên file json nếu tìm thấy, ngược lại trả None.
+    Đầu vào là tên 1 media cần tìm json và tên toàn bộ json dưới dạng set
     """
-
-    # Chuẩn hóa Unicode (rất quan trọng với tiếng Việt)
-    media_filename = unicodedata.normalize("NFC", media_filename)
-    json_files = [unicodedata.normalize("NFC", f) for f in json_files]
-
-    # Bỏ extension media
-
+    # Lấy base name của media
     base_name = Path(media_filename).stem
+    # Lấy extension của media
     media_ext = Path(media_filename).suffix.lower()
 
     # Giảm dần từng ký tự trong media name để tìm
     for i in range(len(base_name), 0, -1):
         prefix = base_name[:i]
-        for json_file in json_files:
+        for json_file in json_dict.keys():
             # Nằm ở đầu
             if json_file.startswith(prefix):
-                try:
-                    json_path = Path(path, json_file)
-                    with open(json_path, "r", encoding="utf-8") as f:
-                        data = json.load(f)
-                except Exception:
-                    continue
-                title = data.get("title")
-                if Path(title).suffix.lower() == media_ext:
+                # json:title
+                if Path(json_dict[json_file]).suffix.lower() == media_ext:
                     return json_file
 
     return None
